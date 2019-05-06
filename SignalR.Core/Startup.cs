278fb9ad69155e602cb4aaaad754711afd42c380
Parker Smart <parker.smart@ncm.com>
2019-05-06 13:45:38 -0600

    Added tutorial code to test .Net Core chat.

--- SignalR.Core/Startup.cs
+++ SignalR.Core/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SignalR.Core.Hubs;
 
 namespace SignalR.Core
 {
@@ -33,6 +34,10 @@ namespace SignalR.Core
 
 
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+			// _________ From Tutorial _________
+
+			services.AddSignalR();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +57,10 @@ namespace SignalR.Core
 			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 			app.UseCookiePolicy();
+			app.UseSignalR(routes =>
+			{
+				routes.MapHub<ChatHub>("/chatHub");
+			});
 
 			app.UseMvc();
 		}
